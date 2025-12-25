e11dd3c15341e95aa0b62e1331a31b71796d61fa
Dilan Kasırga <kasirgadila2001@gmail.com>
2025-12-25 16:05:40 +0300

    Java daire dilimi alanını hesaplama eklendi.

--- src/DaireDilimiAlani.java
+++ src/DaireDilimiAlani.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class DaireDilimiAlani {
     public static void main(String[] args) {
-       double r,a,alan;
+       double r,aci,alan;
        Scanner input = new Scanner(System.in);
 
         System.out.println("Daire dilimi alanının hesaplanması için yarıçap ve merkez açı ölçüsü değerini giriniz.");
         System.out.println("Yarıçap: ");
         r = input.nextDouble();
         System.out.println("Merkez açısı: ");
-        a = input.nextDouble();
+        aci = input.nextDouble();
 
-        alan = (3.14 * (r*r) * a)/360;
+        alan = (3.14 * (r*r) * aci)/360;
         System.out.println("Daire diliminin alanı: " +alan);
 
     }
